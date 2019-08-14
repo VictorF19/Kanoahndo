cdc9e6603004760fadc3017132254ff3c3bc6ade
Mauro Sergio Vapsys Swincik Junior <mauroswincikjr@gmail.com>
2019-08-14 13:47:14 -0300

    PUT em \texto

--- routes/texto.js
+++ routes/texto.js
@@ -89,4 +89,74 @@ router.post('/', async (req, res, next) => {
 
 })
 
+router.put('/', (req, res, next) => {
+
+    if(req.headers["content-type"] != 'application/json')
+    {
+        res.status(406).send("{ 'error': 1, 'msg': 'Content-type must be application/json', 'obj': {} }");
+        return
+    }
+
+    let id_operacao = req.body.id_operacao;
+
+    let texto = req.body.texto;
+
+    let hasIdOperacao = id_operacao != null && id_operacao != undefined && typeof id_operacao == "number"
+
+    let hasTexto = texto != null && texto != undefined && typeof texto == "string" && texto != ""
+
+    if(!hasIdOperacao || !hasTexto)
+    {
+        res.status(406).send("{ 'error': 1, 'msg': 'JSON object must have a numeric id_operacao attribute and a texto attribute', 'obj': {} }");
+        return
+    }
+
+    try
+    {
+
+        if(!fs.existsSync(`${textoPath}${id_operacao}.txt`))
+        {
+            res.status(400).send("{ 'error': 1, 'msg': 'Operation not found', 'obj': {} }")
+            return
+        }
+
+        let textoOriginal = fs.readFileSync(`${textoPath}${id_operacao}.txt`, { encoding: 'latin1' })
+
+        if(textoOriginal.trim() == texto.trim())
+        {
+            res.status(400).send("{ 'error': 1, 'msg': 'No changes detected', 'obj': {} }")
+            return
+        }
+
+        fs.writeFileSync(`${textoPath}${id_operacao}.txt`, texto, { encoding: 'latin1' });
+
+        let responseObject = {
+            error: 0,
+            msg: 'Success',
+            obj: {
+                id_operacao: id_operacao,
+                texto: texto
+            }
+        }
+
+        res.status(200).send(JSON.stringify(responseObject))
+
+    }
+    catch(e)
+    {
+
+        let err = e.stack
+
+        let responseError = {
+            error: 1,
+            msg: err,
+            obj: {}
+        }
+
+        res.status(500).send(JSON.stringify(responseError))
+
+    }
+
+})
+
 module.exports = router;
